f3488d284bc45c04b25a673d89245a06b513ee2e
Hamed <ostovari.eng@gmail.com>
2024-01-07 13:53:45 +0400

    implemented update and delete api

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -51,13 +51,37 @@ exports.createNewTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<...UPDATED TOUR IS HERE>'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'error',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      message: 'Successfully removed'
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'error',
+      message: err
+    });
+  }
+};
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,26 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Tour name is required'],
     unique: true
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
   rate: {
     type: Number,
     default: 4.5
@@ -13,6 +33,30 @@ const tourSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: [true, 'Price is required']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
   }
 });
 
